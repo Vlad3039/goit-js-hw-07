e0c7c4cc6b7d6ceb1299645e232374e3498d5787
Vlad3039 <130600836+Vlad3039@users.noreply.github.com>
2023-09-01 11:26:11 +0200

    hw.7.3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,34 +3,19 @@ import { galleryItems } from "./gallery-items.js";
 // console.log(galleryItems);
 const placeElementRef = document.querySelector(".gallery");
 
-const createGalleryItem = (item) => {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
+const galleryMarkup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+  </a>
+</li>
+`
+  )
+  .join("");
 
-  const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__link");
-  galleryLink.href = item.original;
-
-  const galleryImage = document.createElement("img");
-  galleryImage.classList.add("gallery__image");
-  galleryImage.src = item.preview;
-  galleryImage.setAttribute("data-source", item.original);
-  galleryImage.alt = item.description;
-
-  galleryLink.appendChild(galleryImage);
-  galleryItem.appendChild(galleryLink);
-
-  return galleryItem;
-};
-
-const galleryFragment = document.createDocumentFragment();
-
-galleryItems.forEach((item) => {
-  const galleryItem = createGalleryItem(item);
-  galleryFragment.appendChild(galleryItem);
-});
-
-placeElementRef.appendChild(galleryFragment);
+placeElementRef.innerHTML = galleryMarkup;
 
 let instance = null;
 
@@ -38,15 +23,25 @@ placeElementRef.addEventListener("click", (event) => {
   event.preventDefault();
   if (event.target.classList.contains("gallery__image")) {
     const imageSource = event.target.dataset.source;
-    instance = basicLightbox.create(`
+    instance = basicLightbox.create(
+      `
       <img src="${imageSource}" width="800" height="600">
-    `);
+      `,
+      {
+        onShow: () => {
+          window.addEventListener("keydown", handleKeyPress);
+        },
+        onClose: () => {
+          window.removeEventListener("keydown", handleKeyPress);
+        },
+      }
+    );
     instance.show();
   }
 });
 
-window.addEventListener("keydown", (event) => {
+function handleKeyPress(event) {
   if (event.key === "Escape") {
     instance.close();
   }
-});
+}
